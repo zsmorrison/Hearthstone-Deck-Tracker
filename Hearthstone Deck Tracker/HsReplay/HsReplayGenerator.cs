c27f77d85d3122e907b866f41c49a7c46c4f6a08
Alexander Zeier <epikz37@gmail.com>
2016-02-22 20:25:37 +0100

    hsreplay: added log validation, PTL support and fix for missing "CREATE_GAME"

--- Hearthstone Deck Tracker/HsReplay/HsReplayGenerator.cs
+++ Hearthstone Deck Tracker/HsReplay/HsReplayGenerator.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Hearthstone_Deck_Tracker.Hearthstone;
 using Hearthstone_Deck_Tracker.Stats;
@@ -26,13 +27,17 @@ namespace Hearthstone_Deck_Tracker.HsReplay
 			if(!File.Exists(Msvcr100DllPath))
 				File.Copy(Msvcr100DllHearthstonePath, Msvcr100DllPath);
 
+			var result = ValidateLog(log);
+			if(!result.IsValid)
+				return null;
+
 			using(var sw = new StreamWriter(TmpFilePath))
 			{
 				foreach(var line in log)
 					sw.WriteLine(line);
 			}
 
-			RunExe(stats?.StartTime);
+			RunExe(stats?.StartTime, result.IsPowerTaskList);
 
 			if(new FileInfo(HsReplayOutput).Length == 0)
 			{
@@ -45,14 +50,47 @@ namespace Hearthstone_Deck_Tracker.HsReplay
 			return HsReplayOutput;
 		}
 
-		private static void RunExe(DateTime? time)
+		private static ValidLogResult ValidateLog(List<string> log)
+		{
+			var result = new ValidLogResult {IsValid = log.Count > 0};
+			if(!result.IsValid)
+				return result;
+			result.IsPowerTaskList = log[0].Contains("PowerTaskList.");
+			var createGameLine = -1;
+			for(var i = 0; i < log.Count - 1; i++)
+			{
+				if(log[i].Contains("CREATE_GAME"))
+				{
+					createGameLine = i;
+					break;
+				}
+			}
+			if(createGameLine == -1)
+			{
+				var debugPrintPowerLine = log.FirstOrDefault(x => x.Contains("DebugPrintPower"));
+				if(debugPrintPowerLine == null)
+				{
+					result.IsValid = false;
+					return result;
+				}
+				var createLine = new string(debugPrintPowerLine.TakeWhile(x => x != '-').ToArray()) + "- ";
+				if(result.IsPowerTaskList)
+					createLine += "    ";
+				createLine += "CREATE_GAME";
+				log.Insert(0, createLine);
+			}
+			return result;
+		}
+
+		private static void RunExe(DateTime? time, bool usePowerTaskList)
 		{
 			var dateString = time?.ToString("yyyy-MM-dd");
 			var defaultDateArg = time.HasValue ? $"--default-date={dateString} " : "";
+			var processorArg = usePowerTaskList ? "--processor=PowerTaskList " : "";
 			var procInfo = new ProcessStartInfo
 			{
 				FileName = HsReplayExe,
-				Arguments = defaultDateArg + TmpFilePath,
+				Arguments = defaultDateArg + processorArg + TmpFilePath,
 				CreateNoWindow = true,
 				RedirectStandardOutput = true,
 				UseShellExecute = false
@@ -62,5 +100,11 @@ namespace Hearthstone_Deck_Tracker.HsReplay
 				sw.Write(proc?.StandardOutput.ReadToEnd());
 			proc?.WaitForExit();
 		}
+	}
+
+	public class ValidLogResult
+	{
+		public bool IsValid { get; set; }
+		public bool IsPowerTaskList { get; set; }
 	}
 }
